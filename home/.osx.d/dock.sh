389fda671a024f643d80e558a834fc8d6a7a1e7b
Ryan Sonnek <ryan@codecrate.com>
2014-12-12 15:38:10 -0600

    enable dark theme

--- home/.osx.d/dock.sh
+++ home/.osx.d/dock.sh
@@ -1,8 +1,12 @@
 # Exec this file to set many helpful defaults in OS X
 # Most of this came from https://github.com/mathiasbynens/dotfiles/blob/master/.osx
+# see https://gist.github.com/brandonb927/3195465
 
 # Set the icon size of Dock items to 36 pixels
 defaults write com.apple.dock tilesize -int 36
 
 # Automatically hide and show the Dock
 defaults write com.apple.dock autohide -bool true
+
+# enable OSX Yosemite Dark theme
+defaults write ~/Library/Preferences/.GlobalPreferences AppleInterfaceStyle Dark
