7ce22a92ae914ea152e4d4ccbe987f4933c01870
Ryan Sonnek <ryan@codecrate.com>
2016-05-05 23:31:07 -0500

    add gpg keys

---
+++ home/.profile.d/gpg_agent.sh
@@ -0,0 +1,7 @@
+# see https://github.com/pstadler/keybase-gpg-github
+if test -f ~/.gnupg/.gpg-agent-info -a -n "$(pgrep gpg-agent)"; then
+  source ~/.gnupg/.gpg-agent-info
+  export GPG_AGENT_INFO
+else
+  eval $(gpg-agent --daemon --write-env-file ~/.gnupg/.gpg-agent-info)
+fi
