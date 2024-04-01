ce8f6df30a11179e3b1a722c4bc00fc969743235
Ryan Sonnek <ryan.sonnek@gmail.com>
2024-04-01 13:19:01 -0500

    Add fabric helper files

---
+++ home/.profile.d/fabric.sh
@@ -0,0 +1,3 @@
+# Add Fabric Patterns to path
+# see https://github.com/danielmiessler/fabric
+if [ -f "/Users/ryansonnek/.config/fabric/fabric-bootstrap.inc" ]; then . "/Users/ryansonnek/.config/fabric/fabric-bootstrap.inc"; fi
---
+++ home/.profile.d/pipx.sh
@@ -0,0 +1,2 @@
+# Add scripts installed via pipx to path
+export PATH="$HOME/.local/bin:$PATH"
