d69b1f7b441f9c0107ebb1c83e2dd98d95a38678
Ryan Sonnek <ryan.sonnek@gmail.com>
2024-04-25 12:59:56 -0500

    cleanup conda initializer

--- home/.profile.d/anaconda.sh
+++ home/.profile.d/anaconda.sh
@@ -1,4 +1,34 @@
-# configure anaconda for python development (using homebrew)
-if [ -d "/opt/homebrew/anaconda3" ]; then
-  export PATH=/opt/homebrew/anaconda3/bin:"$PATH"
+# configure anaconda for python development
+# installed via homebrew
+
+# Move the conda initializer out of .zshrc and into this config file
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+__conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.zsh' 'hook' 2> /dev/null)"
+if [ $? -eq 0 ]; then
+    eval "$__conda_setup"
+else
+    if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+        . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+    else
+        export PATH="/opt/homebrew/anaconda3/bin:$PATH"
+    fi
 fi
+unset __conda_setup
+# <<< conda initialize <<<
+
+# IF USING BASH, USE THIS CONFIGURATION INSTEAD
+# >>> conda initialize >>>
+# !! Contents within this block are managed by 'conda init' !!
+# __conda_setup="$('/opt/homebrew/anaconda3/bin/conda' 'shell.bash' 'hook' 2> /dev/null)"
+# if [ $? -eq 0 ]; then
+#     eval "$__conda_setup"
+# else
+#     if [ -f "/opt/homebrew/anaconda3/etc/profile.d/conda.sh" ]; then
+#         . "/opt/homebrew/anaconda3/etc/profile.d/conda.sh"
+#     else
+#         export PATH="/opt/homebrew/anaconda3/bin:$PATH"
+#     fi
+# fi
+# unset __conda_setup
+# <<< conda initialize <<<
