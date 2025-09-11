684bc21012647a121a2868f0eb350e742bd1209a
Ryan Sonnek <ryan.sonnek@gmail.com>
2025-09-11 09:27:26 -0500

    Add scripts directory to path.

--- home/.profile.d/currentbin.sh
+++ home/.profile.d/currentbin.sh
@@ -1,2 +1,3 @@
-# add scripts in the current working directory's bin directory to path
+# add scripts in the current working directory's bin directory (or scripts) 
 export PATH="./bin:$PATH"
+export PATH="./scripts:$PATH"
